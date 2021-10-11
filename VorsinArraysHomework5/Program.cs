56734902cba2b24fad3cf6423a6f6e116864ef8e
voicefromthesky <andrewdoverin@gmail.com>
2021-10-11 12:51:08 +0300

    first commit

---
+++ VorsinArraysHomework5/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace VorsinArraysHomework5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 9, 8, 7, 6, 5, 4, 3, 2, 1 };
+            Array.Sort(array);
+            int length = array.Length;
+            for (int i = 0; i < length; i++)
+            {
+                Console.WriteLine(array[i]);
+            }
+            Console.WriteLine();
+
+            //Homework 6
+
+            Console.WriteLine(Array.IndexOf(array, 5) );
+
+            //Homework 7
+            
+            int[] array2 = { 9, 8, 7, 6, 5, 4, 3, 2, 1 };
+            Array.Sort(array2);
+            int index = Array.IndexOf(array2, 5);
+            array2[index] = 88;
+
+            //Скорее всего я не до конца понял как правильно сделать задачи. НО! Я зделять.
+
+        }
+
+    }
+}
